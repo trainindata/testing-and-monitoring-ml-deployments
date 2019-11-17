a0661b14ce3e17167fc9a703c50402d95369fb53
ChristopherGS <christopher.samiullah@gmail.com>
2019-11-17 09:27:28 +0000

    tooling setup

---
+++ packages/gradient_boosting_model/tests/test_predict.py
@@ -0,0 +1,18 @@
+import math
+
+from gradient_boosting_model.predict import make_prediction
+from gradient_boosting_model.processing.data_management import load_dataset
+from gradient_boosting_model.config.core import config
+
+
+def test_predict():
+    # Given
+    training_data = load_dataset(file_name=config.app_config.training_data_file)
+    test_inputs = training_data.drop(labels="SalePrice", axis="columns")
+
+    # When
+    subject = make_prediction(input_data=test_inputs)
+
+    # Then
+    assert subject is not None
+    assert isinstance(subject.get("predictions")[0], float)
