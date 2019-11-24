dc9bd0e4143bd67c47659c0e626c3c6089e2b386
Christopher Samiullah <ChristopherGS@users.noreply.github.com>
2019-11-24 14:58:01 +0000

    Unit Testing Production ML Code - Test Input Validation

--- packages/gradient_boosting_model/tests/conftest.py
+++ packages/gradient_boosting_model/tests/conftest.py
@@ -21,3 +21,8 @@ def pipeline_inputs():
     )
 
     return X_train, X_test, y_train, y_test
+
+
+@pytest.fixture()
+def sample_input_data():
+    return load_dataset(file_name=config.app_config.test_data_file)
--- packages/gradient_boosting_model/tests/test_pipeline.py
+++ packages/gradient_boosting_model/tests/test_pipeline.py
@@ -1,5 +1,6 @@
 from gradient_boosting_model import pipeline
 from gradient_boosting_model.config.core import config
+from gradient_boosting_model.processing.validation import validate_inputs
 
 
 def test_pipeline_drops_unnecessary_features(pipeline_inputs):
@@ -35,3 +36,19 @@ def test_pipeline_transforms_temporal_features(pipeline_inputs):
         X_transformed.iloc[0]["YearRemodAdd"]
         == X_train.iloc[0]["YrSold"] - X_train.iloc[0]["YearRemodAdd"]
     )
+
+
+def test_pipeline_predict_takes_validated_input(pipeline_inputs, sample_input_data):
+    # Given
+    X_train, X_test, y_train, y_test = pipeline_inputs
+    pipeline.price_pipe.fit(X_train, y_train)
+
+    # When
+    validated_inputs, errors = validate_inputs(input_data=sample_input_data)
+    predictions = pipeline.price_pipe.predict(
+        validated_inputs[config.model_config.features]
+    )
+
+    # Then
+    assert predictions is not None
+    assert errors is None
