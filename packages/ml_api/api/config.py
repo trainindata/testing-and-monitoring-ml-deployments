3c6ac07b8af38114e49ba571899d4d38ed5851a0
Christopher Samiullah <ChristopherGS@users.noreply.github.com>
2019-12-21 10:26:52 +0000

    Shadow Mode ML Code - Asynchronous Implementation

--- packages/ml_api/api/config.py
+++ packages/ml_api/api/config.py
@@ -22,6 +22,7 @@ class Config:
     SERVER_PORT = int(os.getenv("SERVER_PORT", 5000))
     SERVER_HOST = os.getenv("SERVER_HOST", "0.0.0.0")
     LOGGING_LEVEL = os.getenv("LOGGING_LEVEL", logging.INFO)
+    SHADOW_MODE_ACTIVE = os.getenv('SHADOW_MODE_ACTIVE', True)
     SQLALCHEMY_DATABASE_URI = (
         f"postgresql+psycopg2://{os.getenv('DB_USER')}:"
         f"{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
