02e47f5fe91876ecc5b2556c2caa0cc8975cf204
Christopher Samiullah <ChristopherGS@users.noreply.github.com>
2020-01-04 16:18:46 +0000

    Monitoring with Prometheus - Add Simple Metrics

--- exercise_notebooks/prometheus_exercise/app/flask_app.py
+++ exercise_notebooks/prometheus_exercise/app/flask_app.py
@@ -1,19 +1,27 @@
 import prometheus_client
 from flask import Flask
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from app.helpers.middleware import setup_metrics
 
 
 def index():
     return 'home'
 
 
+def foo():
+    return 'foo'
+
+
 def create_app():
     main_app = Flask(__name__)
     main_app.add_url_rule('/', 'index', index)
+    main_app.add_url_rule('/foo', 'foo', foo)
+    setup_metrics(main_app)
 
     # Add prometheus wsgi middleware to route /metrics requests
-    app = DispatcherMiddleware(main_app.wsgi_app, {
-        '/metrics': prometheus_client.make_wsgi_app()
-    })
+    app = DispatcherMiddleware(
+        app=main_app.wsgi_app,
+        mounts={'/metrics': prometheus_client.make_wsgi_app()}
+    )
 
     return app
