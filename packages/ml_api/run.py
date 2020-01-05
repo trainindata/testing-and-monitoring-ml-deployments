5440a7ae930828e0006a671e50859afcaaebae31
Christopher Samiullah <ChristopherGS@users.noreply.github.com>
2020-01-05 13:30:53 +0000

    Monitoring with Prometheus - Instrument Project API

--- packages/ml_api/run.py
+++ packages/ml_api/run.py
@@ -1,3 +1,6 @@
+import prometheus_client
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
 from api.app import create_app
 from api.config import DevelopmentConfig, setup_app_logging
 
@@ -6,8 +9,13 @@ _config = DevelopmentConfig()
 
 # setup logging as early as possible
 setup_app_logging(config=_config)
-application = create_app(config_object=_config).app
+
+main_app = create_app(config_object=_config).app
+application = DispatcherMiddleware(
+        app=main_app.wsgi_app,
+        mounts={'/metrics': prometheus_client.make_wsgi_app()}
+    )
 
 
 if __name__ == "__main__":
-    application.run(port=_config.SERVER_PORT, host=_config.SERVER_HOST)
+    main_app.run(port=_config.SERVER_PORT, host=_config.SERVER_HOST)
